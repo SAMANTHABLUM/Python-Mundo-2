c85d61bd5f1643b4b592d9b6a813c9dfafff5930
Samantha Blum <sambblum@gmail.com>
2025-10-20 16:04:17 -0300

    BANCO

---
+++ Ex71.py
@@ -0,0 +1,29 @@
+print('-'*30)
+print('{:^30}'.format('BANCO BLUM'))
+print('-'*30)
+valor = int(input('Qual valor você deseja sacar? R$'))
+total = valor
+ced = 100
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('='*30)
+print('Volte sempre ao BANCO BLUM! Tenha um excelente dia!')
