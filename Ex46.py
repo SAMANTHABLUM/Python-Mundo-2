bba05ef8c03862f9d68228f3233f235017a98f17
Samantha Blum <sambblum@gmail.com>
2025-10-20 15:48:26 -0300

    Exercício 46

---
+++ Ex46.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('FELIZ ANO NOVO!!!')
+sleep(1)
+print('Vamos fazer uma contagem regressiva para os fogo!!!')
+sleep(1)
+for c in range(10, -1,-1):
+    print('{}'.format(c))
+    sleep(1)
+print('FOGOS DE ARTIFICO 🎆🎇✨💥')
